69ec4c590916f534d8ddae39adb2ff02eca25921
fernandoreinert <reinertpt@gmail.com>
2023-10-20 09:20:55 -0300

    Projeto DIO Spring

---
+++ src/main/java/dio/dio/springboot/challenge/StartApplication.java
@@ -0,0 +1,27 @@
+package dio.dio.springboot.challenge;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import dio.dio.springboot.challenge.model.User;
+import dio.dio.springboot.challenge.service.UserRepository;
+
+@Component
+public class StartApplication implements CommandLineRunner {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setName("GLEYSON");
+        user.setUsername("glysns");
+        user.setPassword("spring-data-jpa");
+        user.setRoles(null);
+        repository.save(user);
+
+        for(User u: repository.findAll()){
+            System.out.println(u);
+        }
+    }
+}
---
+++ src/main/java/dio/dio/springboot/challenge/model/User.java
@@ -0,0 +1,84 @@
+package dio.dio.springboot.challenge.model;
+
+import java.util.List;
+import java.util.Objects;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tab_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String username;
+    @Column(length = 100, nullable = false)
+    private String password;
+    @Transient
+    private List<String> roles;
+
+    public User(){
+    }
+
+    public User(String username){
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+}
